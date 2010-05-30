49d69868d8ccd8f64e1bf1359a5dcbbea7023eaa
Luis Nell <luis@originell.org>
2010-05-30 14:10:48 +0200

    documentation hooray

--- cloud.py
+++ cloud.py
@@ -5,11 +5,41 @@ The code is under GNU GPL V3.0.
 The complete license text should have been shipped with this file. If this is
 not the case you can find it under http://www.gnu.org/licenses/gpl-3.0.txt.
 
-'''cloud.__version_info__''' is a 3-tuple containing the version number.
-(major, minor, maintenance)
+In order to have this fully functional you need:
+    - poster
+        This is needed as Python does not (yet) support
+        multipart/form-data-requests out of the box.
+    - Python 2.7 or ordereddict
+        Necessary because Amazon's S3 expects the file to be the last
+        value in the upload request.
 
-'''cloud.__version__''' is a string generated from __version_info__.
-major.minor.maintenance
+The following values are available:
+    - *cloud.__version_info__*
+        a 3-tuple containing the version number.
+        Format: '(major, minor, maintenance)'
+    - *cloud.__version__*
+        a string generated from __version_info__.
+        Format: 'major.minor.maintenance'
+    - *cloud.PROTOCOL*
+        a string specifying the protocol to be used.
+        Default: '\'http://\''
+    - *cloud.URI*
+        a string containing the URL used by non-authed requests.
+        Default: '\'cl.ly\''
+    - *cloud.AUTH_URI*
+        a string containg the URL used by authed requests.
+        Default: '\'my.cl.ly\''
+    - *cloud.FILE_TYPES*
+        a tuple filled with available filetypes.
+
+The following classes are available:
+    - CloudExcepton
+        An exception thrown on errors with Cloud.
+    - DeleteRequest
+        A HTTP DELETE request.
+    - Cloud
+        The pythonic CloudApp API Wrapper.
+
 """
 
 import urllib2
@@ -53,19 +83,51 @@ class CloudException(Exception):
     pass
 
 class DeleteRequest(urllib2.Request):
-    """A HTTP DELETE request."""
+    """A HTTP DELETE request.
+
+    Public methods:
+        - get_method
+            Sets the HTTP method to DELETE.
+
+    """
     def get_method(self):
         """Sets the HTTP method to DELETE."""
         return 'DELETE'
 
 class Cloud(object):
+    """The pythonic CloudApp API Wrapper.
+
+    Public methods:
+        - auth(username, password)
+            Authenticates a user.
+        - item_info(url)
+            Get metadata about a cl.ly URL.
+        - list_items(page=False, per_page=False, file_type=False, deleted=False)
+            List the authenticated user's items.
+        - create_bookmark(name, url)
+            Creates a bookmark with the given name and url.
+        - upload_file(path)
+            Upload a file.
+            
+    """
     def __init__(self):
+        """
+        Init.
+
+        '''opener''' is for functions that do not need authentication.
+        
+        """
         self.opener = urllib2.build_opener()
         self.opener.addheaders = [('User-Agent', USER_AGENT),
                                   ('Accept', 'application/json'),]
         self.auth_success = 0
 
     def auth(self, username, password):
+        """Authenticate the given username with the given password.
+
+        If poster is installed, build an upload handler.
+
+        """
         if self.auth_success == 1:
             return True
         
@@ -87,6 +149,7 @@ class Cloud(object):
             self._test_auth()
 
     def _test_auth(self):
+        """Test authentication."""
         query = urllib.urlencode({'page': 1, 'per_page': 1})
         page = self.auth_opener.open('%s%s/items?%s' % (PROTOCOL, AUTH_URI, query))
         if page.code == 200:
@@ -95,12 +158,26 @@ class Cloud(object):
         return False
 
     def item_info(self, uri):
+        """Get metadata about a cl.ly URL."""
         validator = '%s%s' % (PROTOCOL, URI)
         if validator in uri:
             return json.load(self.opener.open(uri))
         raise CloudException('URI not valid')
 
     def list_items(self, page=False, per_page=False, file_type=False, deleted=False):
+        """List the authenticated user's items.
+
+        Optional arguments:
+            - *page*
+                an integer representing the page number.
+            - *per_page*
+                an integer representing number of items per page.
+            - *type*
+                Filter items by types found in FILTER_TYPES
+            - *deleted*
+                a boolean. Show trashed items.
+        
+        """
         if self.auth_success == 0:
             raise CloudException('Not authed')
         
@@ -121,6 +198,7 @@ class Cloud(object):
                          encoding='utf-8')
 
     def create_bookmark(self, name, bookmark_uri):
+        """Creates a bookmark with the given name and url."""
         if self.auth_success == 0:
             raise CloudException('Not authed')
 
@@ -132,6 +210,14 @@ class Cloud(object):
         return json.load(self.auth_opener.open(request))
 
     def upload_file(self, path):
+        """Upload a file.
+
+        This function requires you to be authenticated.
+        
+        Furthermore you need to have poster installed as well was python 2.7 or
+        ordereddict.
+        
+        """
         if not POSTER:
             raise CloudException('Poster is not installed')
         if not ORDERED_DICT:
@@ -157,6 +243,7 @@ class Cloud(object):
         return json.load(self.upload_auth_opener.open(request))
 
     def delete_file(self, href):
+        """Delete a file with the given href."""
         if self.auth_success == 0:
             raise CloudException('Not authed')
         result = self.auth_opener.open(DeleteRequest(href))
